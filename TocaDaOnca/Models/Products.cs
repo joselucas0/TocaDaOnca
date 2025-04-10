9b3c91dc3634726c554b4a9700e0523972f357ba
Willdanthe <will.allaman@hotmail.com>
2025-04-10 06:22:25 -0400

    Feat: Model Products

--- TocaDaOnca/Models/Products.cs
+++ TocaDaOnca/Models/Products.cs
@@ -9,6 +9,40 @@ namespace TocaDaOnca.Models
 {
     public class Products
     {
+        [Column("id")]
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Column("name")]
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; } = string.Empty;
+        
+        [Column("description")]
+        [Required]
+        [StringLength(512)]
+        public string Description { get; set; } = string.Empty;
+        
+        [Column("cost")]
+        [Required]
+        public float Cost { get; set; }
+        
+        [Column("price")]
+        [Required]
+        public float Price { get; set; }
+
+        [Column("stock")]
+        [Required]
+        public int Stock { get; set; }
+
+        [Column("created_at")]
+        [Required]
+        public DateTime Created_at { get; set; } = DateTime.Now;
+        
+        [Column("updated_at")]
+        [Required]
+        public DateTime Updated_at { get; set; } = DateTime.Now;
         
     }
 }
