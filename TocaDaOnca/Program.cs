dee9851cb803ed026ca9a678239aaa90c0e16b4a
Diogoltt <diogoleite09@gmail.com>
2025-04-11 12:36:50 -0300

    feat: add jwt configurations

--- TocaDaOnca/Program.cs
+++ TocaDaOnca/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using TocaDaOnca.AppDbContext;
+using TocaDaOnca.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -12,9 +17,30 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<Context>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Adicionar configuração JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+// Registrar serviços de autenticação e token
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<TokenService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -23,11 +49,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// Add middleware for routing and authorization
+// Adicionar middleware para autenticação e autorização
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
-// Map controller endpoints
 app.MapControllers();
 
 app.Run();
